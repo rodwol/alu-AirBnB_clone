8dc643ae397e35762bd29d57662c203373853953
Angelo Verlain <geoangercola@gmail.com>
2024-09-05 22:19:38 +0200

    implement kwargs in base_model

--- models/base_model.py
+++ models/base_model.py
@@ -15,12 +15,20 @@ class BaseModel:
     in the project
     """
 
-    def __init__(self):
+    def __init__(self,  *args, **kwargs):
         """Initialize a new BaseModel."""
 
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+
+        if kwargs:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], time_format)
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], time_format)
+            self.__dict__.update(kwargs)
+        else:
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
